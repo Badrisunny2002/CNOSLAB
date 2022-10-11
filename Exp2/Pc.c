bcce8ce25ad6eeee4475458c6dff0b2b57e0dc5f
204g1a0518 <204g1a0518@srit.ac.in>
2022-10-11 12:45:39 +0530

    successfully executed

--- Exp2/Pc.c
+++ Exp2/Pc.c
@@ -7,35 +7,36 @@
 #include<stdio.h>
 void main()
 {
-int buffer[10], bufsize, in, out, produce, consume,
-choice=0; in = 0;
+int buffer[10], bufsize, in, out, produce, consume,choice=0; in = 0;
 out = 0;
 bufsize = 10;
 while(choice !=3)
 {
-printf(“\n1. Produce \t 2. Consume \t3. Exit”);
-printf(“\nEnter your choice: ”);
-scanf(“%d”,&choice);
+printf("\n1. Produce \t 2. Consume \t3. Exit");
+printf("\nEnter your choice: ");
+scanf("%d",&choice);
 switch(choice) {
 
 case 1: if((in+1)%bufsize==out)
-printf(“\nBuffer is Full”);
+printf("\nBuffer is Full");
 else
 {
-break;
-}
-printf(“\nEnter the value: “);
-scanf(“%d”, &produce);
+
+printf("\nEnter the value: ");
+scanf("%d", &produce);
 buffer[in] = produce;
 in = (in+1)%bufsize;
 break;
+}
 case 2: if(in == out)
-printf(“\nBuffer is Empty”);
+printf("\nBuffer is Empty");
 else
 {
 consume = buffer[out];
-printf(“\nThe consumed value is %d”, consume);
+printf("\nThe consumed value is %d", consume);
 out = (out+1)%bufsize;
 }
 break;
-} } }
+}
+ } 
+}
