b79c64cb672021d2e94ea31039de2e720f89e0ea
204g1a0518 <204g1a0518@srit.ac.in>
2022-10-11 11:23:21 +0530

    successfully executed

--- Exp1/RR.c
+++ Exp1/RR.c
@@ -5,11 +5,11 @@
    Date of Execution: 13-09-2022
 */
 #include<stdio.h>
-main()
+ void main()
 {
 int i,j,n,bu[10],wa[10],tat[10],t,ct[10],max;
 float awt=0,att=0,temp=0;
-clrscr();
+
 printf("Enter the no of processes -- ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -46,4 +46,4 @@ printf("\nThe Average Waiting time is -- %f ",awt/n);
 printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\t%d \t %d \t\t %d \t\t %d \n",i+1,ct[i],wa[i],tat[i]);
-getch();}
+}
--- Exp1/Sjf.c
+++ Exp1/Sjf.c
@@ -5,12 +5,11 @@
    Date of Execution: 13-09-2022
 */
 #include<stdio.h>
-#include<conio.h>
-main()
+ void main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
-clrscr();
+
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -43,4 +42,5 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); getch();}
+printf("\nAverage Turnaround Time -- %f", tatavg/n); 
+}
