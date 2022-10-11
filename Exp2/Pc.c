57e3bad9c5de255fc933ec505b14ddd39292e757
204G1A0518 <94513107+Badrisunny2002@users.noreply.github.com>
2022-10-11 11:33:53 +0530

    M.Badrinath created Pc.c

---
+++ Exp2/Pc.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.Badrinath
+   Roll No: 204G1A0518
+   Title of the Experiment: CPU Scheduling Using producer-consumer Problem
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
+*/
